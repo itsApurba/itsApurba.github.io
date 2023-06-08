f0f24d1993dd62a48a01e2a09c7db7b01e5cd13d
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2023-06-08 18:49:04 +0000

    update github-readme-activity-graph

--- src/components/activity/Graph.jsx
+++ src/components/activity/Graph.jsx
@@ -8,7 +8,7 @@ const Graph = () => {
         w={"xl"}
         // loading='lazy'
         alt='Apurba Adhikary'
-        src='https://github-readme-activity-graph.cyclic.app/graph?username=itsapurba&bg_color=000000&color=ae9f37&line=578e62&point=137000&area=true&hide_border=true'
+        src='https://github-readme-activity-graph.vercel.app/graph?username=itsapurba&bg_color=000000&color=ae9f37&line=578e62&point=137000&area=true&hide_border=true'
       />
     </Link>
   );
